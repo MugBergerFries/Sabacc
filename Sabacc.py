29b6ec8695930e08f008b8590c4a2ae0476a81ef
BuildTools <digitallyApocalyptic@users.noreply.github.com>
2016-09-07 10:46:13 -0600

    initial repo setup

---
+++ Sabacc.py
@@ -0,0 +1,16 @@
+
+
+for i in range()
+
+deck = []
+
+deal()
+
+deckdict = {"Queen of Air and Darkness":2}
+
+print deckdict["Queen of Air and Darkness"]
+
+def deal():
+	# some code
+
+def handTotal():
